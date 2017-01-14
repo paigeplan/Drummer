80680d2f31f8bf6d50a45afe51b30fd4c03cacd3
Paige Plander <planderpaige@gmail.com>
2017-01-13 16:03:55 -0800

    Updating README and UI

--- Drummer/DrumPadViewController.swift
+++ Drummer/DrumPadViewController.swift
@@ -32,5 +32,20 @@ class DrumPadViewController: UIViewController {
     @IBAction func segmentedControlDidChange(_ sender: UISegmentedControl) {
         drummer.setDrumKitID(toValue: sender.selectedSegmentIndex)
     }
+    
+
+}
+
+extension UIImage {
+    static func fromColor(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
+    }
 }
 
